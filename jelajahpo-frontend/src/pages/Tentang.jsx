b9a17e779898bc50220b9def495293a00bd84a4a
imaaa86 <imaa8246@gmail.com>
2026-09-01 21:34:07 +0700

    penambahan menu tentang

---
+++ jelajahpo-frontend/src/pages/Tentang.jsx
@@ -0,0 +1,20 @@
+//Tentang.jsx
+export default function Tentang() {
+    return (
+        <div>
+            <h2>Tentang Saya</h2>
+
+            <p>
+                Halo, perkenalkan nama saya <b>Isma Dian Hidayah</b>
+            </p>
+
+            <p>
+                Saya adalah siswa kelas <b>XII RPL 1</b>
+            </p>
+
+            <p>
+                Destinasi wisata favorit yang paling ingin saya kunjungi adalah <b>Ranu Kumbolo</b>
+            </p>
+        </div>
+    )
+}
